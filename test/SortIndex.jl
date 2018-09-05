a43f103b9f8b038437189789f96e5f49dc818862
Andy Ferris <andy.ferris@roames.com.au>
2018-09-05 21:03:38 +1000

    More tests for sort predicates

--- test/SortIndex.jl
+++ test/SortIndex.jl
@@ -7,8 +7,28 @@
     @test issetequal(findall(isequal(3.0), b), [1, 4])
 
     @test issetequal(findall(isless(1.1), b), [])
+    @test issetequal(findall(isless(2.0), b), [])
     @test issetequal(findall(isless(2.1), b), [2])
     @test issetequal(findall(isless(3.1), b), [1, 2, 4])
+    @test issetequal(findall(isless(8.1), b), [1, 2, 3, 4])
+
+    @test issetequal(findall(islessequal(1.1), b), [])
+    @test issetequal(findall(islessequal(2.0), b), [2])
+    @test issetequal(findall(islessequal(2.1), b), [2])
+    @test issetequal(findall(islessequal(3.1), b), [1, 2, 4])
+    @test issetequal(findall(islessequal(8.1), b), [1, 2, 3, 4])
+
+    @test issetequal(findall(isgreater(1.1), b), [1, 2, 3, 4])
+    @test issetequal(findall(isgreater(2.0), b), [1, 3, 4])
+    @test issetequal(findall(isgreater(2.1), b), [1, 3, 4])
+    @test issetequal(findall(isgreater(3.1), b), [3])
+    @test issetequal(findall(isgreater(8.1), b), [])
+
+    @test issetequal(findall(isgreaterequal(1.1), b), [1, 2, 3, 4])
+    @test issetequal(findall(isgreaterequal(2.0), b), [1, 2, 3, 4])
+    @test issetequal(findall(isgreaterequal(2.1), b), [1, 3, 4])
+    @test issetequal(findall(isgreaterequal(3.1), b), [3])
+    @test issetequal(findall(isgreaterequal(8.1), b), [])
 
     @test issetequal(findall(in(1.1..1.9), b), [])
     @test issetequal(findall(in(1.1..2.1), b), [2])
@@ -20,8 +40,28 @@
     @test filter(isequal(3.0), b) == [3.0, 3.0]
 
     @test issetequal(filter(isless(1.1), b), [])
+    @test issetequal(filter(isless(2.0), b), [])
     @test issetequal(filter(isless(2.1), b), [2.0])
     @test issetequal(filter(isless(3.1), b), [2.0, 3.0, 3.0])
+    @test issetequal(filter(isless(8.1), b), [2.0, 3.0, 3.0, 8.0])
+
+    @test issetequal(filter(islessequal(1.1), b), [])
+    @test issetequal(filter(islessequal(2.0), b), [2.0])
+    @test issetequal(filter(islessequal(2.1), b), [2.0])
+    @test issetequal(filter(islessequal(3.1), b), [2.0, 3.0, 3.0])
+    @test issetequal(filter(islessequal(8.1), b), [2.0, 3.0, 3.0, 8.0])
+
+    @test issetequal(filter(isgreater(1.1), b), [2.0, 3.0, 3.0, 8.0])
+    @test issetequal(filter(isgreater(2.0), b), [3.0, 3.0, 8.0])
+    @test issetequal(filter(isgreater(2.1), b), [3.0, 3.0, 8.0])
+    @test issetequal(filter(isgreater(3.1), b), [8.0])
+    @test issetequal(filter(isgreater(8.1), b), [])
+
+    @test issetequal(filter(isgreaterequal(1.1), b), [2.0, 3.0, 3.0, 8.0])
+    @test issetequal(filter(isgreaterequal(2.0), b), [2.0, 3.0, 3.0, 8.0])
+    @test issetequal(filter(isgreaterequal(2.1), b), [3.0, 3.0, 8.0])
+    @test issetequal(filter(isgreaterequal(3.1), b), [8.0])
+    @test issetequal(filter(isgreaterequal(8.1), b), [])
 
     @test issetequal(filter(in(1.1..1.9), b), [])
     @test issetequal(filter(in(1.1..2.1), b), [2.0])
@@ -34,8 +74,28 @@
     @test issetequal(findall(isequal(3.0), c), [2, 3])
 
     @test issetequal(findall(isless(1.1), c), [])
+    @test issetequal(findall(isless(2.0), c), [])
     @test issetequal(findall(isless(2.1), c), [1])
     @test issetequal(findall(isless(3.1), c), [1, 2, 3])
+    @test issetequal(findall(isless(8.1), c), [1, 2, 3, 4])
+
+    @test issetequal(findall(islessequal(1.1), c), [])
+    @test issetequal(findall(islessequal(2.0), c), [1])
+    @test issetequal(findall(islessequal(2.1), c), [1])
+    @test issetequal(findall(islessequal(3.1), c), [1, 2, 3])
+    @test issetequal(findall(islessequal(8.1), c), [1, 2, 3, 4])
+
+    @test issetequal(findall(isgreater(1.1), c), [1, 2, 3, 4])
+    @test issetequal(findall(isgreater(2.0), c), [2, 3, 4])
+    @test issetequal(findall(isgreater(2.1), c), [2, 3, 4])
+    @test issetequal(findall(isgreater(3.1), c), [4])
+    @test issetequal(findall(isgreater(8.1), c), [])
+
+    @test issetequal(findall(isgreaterequal(1.1), c), [1, 2, 3, 4])
+    @test issetequal(findall(isgreaterequal(2.0), c), [1, 2, 3, 4])
+    @test issetequal(findall(isgreaterequal(2.1), c), [2, 3, 4])
+    @test issetequal(findall(isgreaterequal(3.1), c), [4])
+    @test issetequal(findall(isgreaterequal(8.1), c), [])
 
     @test issetequal(findall(in(1.1..1.9), c), [])
     @test issetequal(findall(in(1.1..2.1), c), [1])
@@ -47,8 +107,28 @@
     @test filter(isequal(3.0), c) == [3.0, 3.0]
 
     @test issetequal(filter(isless(1.1), c), [])
+    @test issetequal(filter(isless(2.0), c), [])
     @test issetequal(filter(isless(2.1), c), [2.0])
     @test issetequal(filter(isless(3.1), c), [2.0, 3.0, 3.0])
+    @test issetequal(filter(isless(8.1), c), [2.0, 3.0, 3.0, 8.0])
+
+    @test issetequal(filter(islessequal(1.1), c), [])
+    @test issetequal(filter(islessequal(2.0), c), [2.0])
+    @test issetequal(filter(islessequal(2.1), c), [2.0])
+    @test issetequal(filter(islessequal(3.1), c), [2.0, 3.0, 3.0])
+    @test issetequal(filter(islessequal(8.1), c), [2.0, 3.0, 3.0, 8.0])
+
+    @test issetequal(filter(isgreater(1.1), c), [2.0, 3.0, 3.0, 8.0])
+    @test issetequal(filter(isgreater(2.0), c), [3.0, 3.0, 8.0])
+    @test issetequal(filter(isgreater(2.1), c), [3.0, 3.0, 8.0])
+    @test issetequal(filter(isgreater(3.1), c), [8.0])
+    @test issetequal(filter(isgreater(8.1), c), [])
+
+    @test issetequal(filter(isgreaterequal(1.1), c), [2.0, 3.0, 3.0, 8.0])
+    @test issetequal(filter(isgreaterequal(2.0), c), [2.0, 3.0, 3.0, 8.0])
+    @test issetequal(filter(isgreaterequal(2.1), c), [3.0, 3.0, 8.0])
+    @test issetequal(filter(isgreaterequal(3.1), c), [8.0])
+    @test issetequal(filter(isgreaterequal(8.1), c), [])
 
     @test issetequal(filter(in(1.1..1.9), c), [])
     @test issetequal(filter(in(1.1..2.1), c), [2.0])
--- test/UniqueSortIndex.jl
+++ test/UniqueSortIndex.jl
@@ -6,9 +6,29 @@
     @test findall(isequal(8.0), b)::MaybeVector == [3]
     @test findall(isequal(2.0), b)::MaybeVector == [2]
 
-    @test findall(isless(1.1), b) == []
-    @test findall(isless(2.1), b) == [2]
+    @test issetequal(findall(isless(1.1), b), [])
+    @test issetequal(findall(isless(2.0), b), [])
+    @test issetequal(findall(isless(2.1), b), [2])
     @test issetequal(findall(isless(3.1), b), [1, 2])
+    @test issetequal(findall(isless(8.1), b), [1, 2, 3, 4])
+
+    @test issetequal(findall(islessequal(1.1), b), [])
+    @test issetequal(findall(islessequal(2.0), b), [2])
+    @test issetequal(findall(islessequal(2.1), b), [2])
+    @test issetequal(findall(islessequal(3.1), b), [1, 2])
+    @test issetequal(findall(islessequal(8.1), b), [1, 2, 3, 4])
+
+    @test issetequal(findall(isgreater(1.1), b), [1, 2, 3, 4])
+    @test issetequal(findall(isgreater(2.0), b), [1, 3, 4])
+    @test issetequal(findall(isgreater(2.1), b), [1, 3, 4])
+    @test issetequal(findall(isgreater(3.1), b), [3, 4])
+    @test issetequal(findall(isgreater(8.1), b), [])
+
+    @test issetequal(findall(isgreaterequal(1.1), b), [1, 2, 3, 4])
+    @test issetequal(findall(isgreaterequal(2.0), b), [1, 2, 3, 4])
+    @test issetequal(findall(isgreaterequal(2.1), b), [1, 3, 4])
+    @test issetequal(findall(isgreaterequal(3.1), b), [3, 4])
+    @test issetequal(findall(isgreaterequal(8.1), b), [])
 
     @test issetequal(findall(in(1.1..1.9), b), [])
     @test issetequal(findall(in(1.1..2.1), b), [2])
@@ -19,9 +39,29 @@
     @test filter(isequal(8.0), b)::MaybeVector == [8.0]
     @test filter(isequal(2.0), b)::MaybeVector == [2.0]
 
-    @test filter(isless(1.1), b) == []
-    @test filter(isless(2.1), b) == [2.0]
+    @test issetequal(filter(isless(1.1), b), [])
+    @test issetequal(filter(isless(2.0), b), [])
+    @test issetequal(filter(isless(2.1), b), [2.0])
     @test issetequal(filter(isless(3.1), b), [2.0, 3.0])
+    @test issetequal(filter(isless(8.1), b), [2.0, 3.0, 5.0, 8.0])
+
+    @test issetequal(filter(islessequal(1.1), b), [])
+    @test issetequal(filter(islessequal(2.0), b), [2.0])
+    @test issetequal(filter(islessequal(2.1), b), [2.0])
+    @test issetequal(filter(islessequal(3.1), b), [2.0, 3.0])
+    @test issetequal(filter(islessequal(8.1), b), [2.0, 3.0, 5.0, 8.0])
+
+    @test issetequal(filter(isgreater(1.1), b), [2.0, 3.0, 5.0, 8.0])
+    @test issetequal(filter(isgreater(2.0), b), [3.0, 5.0, 8.0])
+    @test issetequal(filter(isgreater(2.1), b), [3.0, 5.0, 8.0])
+    @test issetequal(filter(isgreater(3.1), b), [5.0, 8.0])
+    @test issetequal(filter(isgreater(8.1), b), [])
+
+    @test issetequal(filter(isgreaterequal(1.1), b), [2.0, 3.0, 5.0, 8.0])
+    @test issetequal(filter(isgreaterequal(2.0), b), [2.0, 3.0, 5.0, 8.0])
+    @test issetequal(filter(isgreaterequal(2.1), b), [3.0, 5.0, 8.0])
+    @test issetequal(filter(isgreaterequal(3.1), b), [5.0, 8.0])
+    @test issetequal(filter(isgreaterequal(8.1), b), [])
 
     @test issetequal(filter(in(1.1..1.9), b), [])
     @test issetequal(filter(in(1.1..2.1), b), [2.0])
@@ -34,9 +74,29 @@
     @test findall(isequal(8.0), c)::MaybeVector == [4]
     @test findall(isequal(2.0), c)::MaybeVector == [1]
 
-    @test findall(isless(1.1), c) == []
-    @test findall(isless(2.1), c) == [1]
+    @test issetequal(findall(isless(1.1), c), [])
+    @test issetequal(findall(isless(2.0), c), [])
+    @test issetequal(findall(isless(2.1), c), [1])
     @test issetequal(findall(isless(3.1), c), [1, 2])
+    @test issetequal(findall(isless(8.1), c), [1, 2, 3, 4])
+
+    @test issetequal(findall(islessequal(1.1), c), [])
+    @test issetequal(findall(islessequal(2.0), c), [1])
+    @test issetequal(findall(islessequal(2.1), c), [1])
+    @test issetequal(findall(islessequal(3.1), c), [1, 2])
+    @test issetequal(findall(islessequal(8.1), c), [1, 2, 3, 4])
+
+    @test issetequal(findall(isgreater(1.1), c), [1, 2, 3, 4])
+    @test issetequal(findall(isgreater(2.0), c), [2, 3, 4])
+    @test issetequal(findall(isgreater(2.1), c), [2, 3, 4])
+    @test issetequal(findall(isgreater(3.1), c), [3, 4])
+    @test issetequal(findall(isgreater(8.1), c), [])
+
+    @test issetequal(findall(isgreaterequal(1.1), c), [1, 2, 3, 4])
+    @test issetequal(findall(isgreaterequal(2.0), c), [1, 2, 3, 4])
+    @test issetequal(findall(isgreaterequal(2.1), c), [2, 3, 4])
+    @test issetequal(findall(isgreaterequal(3.1), c), [3, 4])
+    @test issetequal(findall(isgreaterequal(8.1), c), [])
 
     @test issetequal(findall(in(1.1..1.9), c), [])
     @test issetequal(findall(in(1.1..2.1), c), [1])
@@ -47,9 +107,29 @@
     @test filter(isequal(8.0), c)::MaybeVector == [8.0]
     @test filter(isequal(2.0), c)::MaybeVector == [2.0]
 
-    @test filter(isless(1.1), c) == []
-    @test filter(isless(2.1), c) == [2.0]
+    @test issetequal(filter(isless(1.1), c), [])
+    @test issetequal(filter(isless(2.0), c), [])
+    @test issetequal(filter(isless(2.1), c), [2.0])
     @test issetequal(filter(isless(3.1), c), [2.0, 3.0])
+    @test issetequal(filter(isless(8.1), c), [2.0, 3.0, 5.0, 8.0])
+
+    @test issetequal(filter(islessequal(1.1), c), [])
+    @test issetequal(filter(islessequal(2.0), c), [2.0])
+    @test issetequal(filter(islessequal(2.1), c), [2.0])
+    @test issetequal(filter(islessequal(3.1), c), [2.0, 3.0])
+    @test issetequal(filter(islessequal(8.1), c), [2.0, 3.0, 5.0, 8.0])
+
+    @test issetequal(filter(isgreater(1.1), c), [2.0, 3.0, 5.0, 8.0])
+    @test issetequal(filter(isgreater(2.0), c), [3.0, 5.0, 8.0])
+    @test issetequal(filter(isgreater(2.1), c), [3.0, 5.0, 8.0])
+    @test issetequal(filter(isgreater(3.1), c), [5.0, 8.0])
+    @test issetequal(filter(isgreater(8.1), c), [])
+
+    @test issetequal(filter(isgreaterequal(1.1), c), [2.0, 3.0, 5.0, 8.0])
+    @test issetequal(filter(isgreaterequal(2.0), c), [2.0, 3.0, 5.0, 8.0])
+    @test issetequal(filter(isgreaterequal(2.1), c), [3.0, 5.0, 8.0])
+    @test issetequal(filter(isgreaterequal(3.1), c), [5.0, 8.0])
+    @test issetequal(filter(isgreaterequal(8.1), c), [])
 
     @test issetequal(filter(in(1.1..1.9), c), [])
     @test issetequal(filter(in(1.1..2.1), c), [2.0])
